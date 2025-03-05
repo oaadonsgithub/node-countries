bf0700df8e913151aa9e55404865c5a0bf77cf09
oaadonsgithub <77804816+oaadonsgithub@users.noreply.github.com>
2025-03-05 11:17:45 -0800

    first commit

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 var config = {
 	database: {
-		// host:	  'localhost', 		// database host
-		host:	  '127.0.0.1', 		// database host
-		user: 	  'root', 			// your database username
-		password: 'root', 			// your database password
-		port: 	  3306, 			// default MySQL port
-		db: 	  'node_countries' // your database name
+			// host:          'localhost',          // database host
+			host:     'database-03.cnyk4ek4wokb.us-west-1.rds.amazonaws.com',               // database host
+			user:     'SQladmin',                   // your database username
+			password: 'AstR8i76TY<8',                       // your database password
+			port:     3306,                         // default MySQL port
+			db:       'node_countries' // your database name
 	},
 	server: {
-		host:'127.0.0.1', // the host for the server
-		port: '3000' // the port for the server
+			host:'127.0.0.1', // the host for the server
+			port: '3000' // the port for the server
 	}
 }
 
